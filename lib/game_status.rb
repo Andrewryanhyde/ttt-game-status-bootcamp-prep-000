9034faa627a167830938a2fb61a4e9c69f3c2fd4
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 09:17:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,4 +18,7 @@ WIN_COMBINATIONS = [
 
 def won(board)
   WIN_COMBINATIONS.each do |combination|
-    win_index_1 = 
+    win_index_1 = combination[0]
+    win_index_2 = combination[1]
+    win_index_3 = combination[2]
+    
