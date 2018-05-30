a26dd10dcce83288fd51b2731930260ab705d01b
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 10:48:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,7 +51,7 @@ end
 
 
 def over?(board) 
-  if won?(board) == true || draw?(board) == true || full?(board) == true 
+  if won?(board) || draw?(board) == true || full?(board) == true 
     return true 
   else 
     return false
