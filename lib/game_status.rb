bd52e212c357e1f11aaf7c11e47b19fda2dd019d
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 10:14:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,18 +18,18 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each {|combination|
-    win_index_1 = combination[0]
-    win_index_2 = combination[1]
-    win_index_3 = combination[2]
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
     
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return combination
+      return win_combination
     elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return combination
+      return win_combination
     end
   }
   return false
