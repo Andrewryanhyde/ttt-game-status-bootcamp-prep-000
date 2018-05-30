26f07672ac6453c40e8ca0f9d7656d3e3f324fb8
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 10:36:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,13 +22,13 @@ def won?(board)
     win_index_1 = win_combination[1]
     win_index_2 = win_combination[2]
     
-    position_0 = board[win_index_0]
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
+    position_1 = board[win_index_0]
+    position_2 = board[win_index_1]
+    position_3 = board[win_index_2]
     
-    if position_0 == "X" && position_1 == "X" && position_2 == "X"
+    if position_1 == "X" && position_2 == "X" && position_2 == "X"
       return win_combination
-    elseif position_0 == "O" && position_1 == "O" && position_2 == "O"
+    elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
   }
