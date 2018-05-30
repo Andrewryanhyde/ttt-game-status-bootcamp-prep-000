5eb646c2387952fe9518d8f82aa6399463510a0b
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 09:39:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,11 +27,9 @@ def won?(board)
     position_3 = board[win_index_3]
     
     if position_1 == "X" && position_2 = "X" && position_3 = "X"
-      return true
+      return combination
     elseif position_1 == "O" && position_2 = "O" && position_3 = "O"
-      return true
-    elseif position_1 == "O" && position_2 = "O" && position_3 = "O" && position_1 == "X" && position_2 = "X" && position_3 = "X"
-      return false
+      return combination
     else 
       return false 
     end
