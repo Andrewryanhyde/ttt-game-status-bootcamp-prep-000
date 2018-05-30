d9e471e86c233447f987f162a26c536b4e3f273e
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 10:10:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,8 +30,6 @@ def won?(board)
       return combination
     elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return combination
-    else 
-      return false
     end
   }
   return false
