a9444905b21057eaf4ff56a47ba70b7412187eec
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 10:33:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,10 +28,11 @@ def won?(board)
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
-    else 
-      false
+    elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
     end
   }
+  return false
 end
 
 def full?(board)
